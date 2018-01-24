2f4a6f9770650c495edaeb69ce58d7bb134c2ec7
Brooke Mosby <brookemosby@hotmail.com>
2018-01-23 17:24:49 -0700

    adding more data from imdb

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,21 @@
+
+
+
+#include <iostream>
+
+	using namespace std;
+
+int main()
+{
+	int inputted_value = 0;
+
+	while (inputted_value != -1)
+	{
+		cin >> inputted_value;
+		cout << inputted_value << endl;
+	}
+
+
+	system("pause");
+	return 0;
+}
